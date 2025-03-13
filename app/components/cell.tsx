f5743cc7735b49ffa622cacdb81c2faf17a761c2
Imobach Martín <martin@365farmnet.com>
2025-03-13 11:29:04 -0300

    fix: avoid setting colors directly as it affects other themes

--- app/components/cell.tsx
+++ app/components/cell.tsx
@@ -11,7 +11,7 @@ export interface BoardCellProps {
 };
 export const BoardCell = memo(({ cell, dataTestId, x, y, onClick, color }: BoardCellProps) => {
   return <div data-testid={dataTestId}
-    className={`flex cursor-pointer hover:border-blue-500 hover:border-4 justify-center items-center border border-white w-8 h-8 ${color}`} onClick={() => onClick([x, y])}>
+    className={`flex cursor-pointer hover:border-blue-500 hover:border-4 justify-center items-center border w-8 h-8 ${color}`} onClick={() => onClick([x, y])}>
     {cell}
 </div>;
 });
